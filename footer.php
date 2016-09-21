9d422f6854527d739353d70653dd88e0cdf2fbc5
Hyce <jon.poullain@gmail.com>
2016-09-21 18:09:02 +0200

    Maj footer/head/navbar for artist

--- footer.php
+++ footer.php
@@ -34,3 +34,14 @@
             </div>
         </div>
     </footer>
+
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+<script src="js/index.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="js/clean-blog.min.js"></script>
--- home.php
+++ home.php
@@ -1,39 +1,6 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>DreamNation</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/clean-blog.min.css" rel="stylesheet">
-    <!-- Custom Fonts -->
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-
-    <link href="css/style.css" rel="stylesheet">
-
-    <link rel="icon" href="img/logo-dreamnation.ico" />
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js" type="text/javascript"></script>
-        <script type="text/javascript" charset="utf-8" src="js/jquery.tubular.1.0.js"></script>
-    <![endif]-->
-
-</head>
+<?php
+include "head.php";
+?>
 
 <body>
 
@@ -121,16 +88,6 @@
     <?php
     include "footer.php";
     ?>
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-    <script src="js/index.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="js/clean-blog.min.js"></script>
 
 </body>
 
