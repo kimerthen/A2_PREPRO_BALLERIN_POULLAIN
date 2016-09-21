d578716e69809104969c52648581655ae58cdd2a
Vincent.B <vincent.ballerin@hotmail.fr>
2016-09-21 15:24:35 +0200

    Create footer.php & include to home

--- home.php
+++ home.php
@@ -116,44 +116,9 @@
         </div>
     </div>
 
-    <hr>
-
-    <!-- Footer -->
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                    <ul class="list-inline text-center">
-                        <li>
-                            <a href="https://twitter.com/Paris">
-                                <span class="fa-stack fa-lg">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
-                                </span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.facebook.com/pages/Liste-des-ponts-de-Paris/760157047345556?fref=ts">
-                                <span class="fa-stack fa-lg">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
-                                </span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <span class="fa-stack fa-lg">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-github fa-stack-1x fa-inverse"></i>
-                                </span>
-                            </a>
-                        </li>
-                    </ul>
-                    <p class="copyright text-muted">Copyright &copy; jonathan-poullain.com 2015</p>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <?php
+    include "footer.php";
+    ?>
 
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
