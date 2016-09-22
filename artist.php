ec59dc3e09ce94441bc66af6a2bd05f75877605a
Vincent.B <vincent.ballerin@hotmail.fr>
2016-09-22 15:39:54 +0200

    Fix error double header

--- artist.php
+++ artist.php
@@ -11,7 +11,7 @@ include "head.php";
 ?>
 
 
-<header class="intro-header" style="background-image: url('img/banner.png')">
+<header class="intro-header" style="background-image: url('img/banner.png ')">
     <div class="container">
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
