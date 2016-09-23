0cc99d0d77747f33035448ad263b83765aaaa48b
Kimerthen <vincent.ballerin@hotmail.fr>
2016-09-23 11:44:09 +0200

    Fix error background home.php

--- home.php
+++ home.php
@@ -16,74 +16,64 @@ include "head.php";
         </video>
     </header>
 
-    <div class="tick">
-        <div class="cover">
-
-        </div>
-
-    </div>
 
     <!-- Main Content -->
-    <div class="container" id="container">
-        <div class="row">
-            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                <div class="post-preview">
-                </div>
-                <hr>
-                <div class="post-preview">
-                    <div class="art" >
-                    <a href="arts.html">
-                        <h2 class="post-title">
-                            Early Tickets
-                        </h2>
-                        <h3 class="post-subtitle">
-                            Jusqu'au 27/09
-                        </h3>
-                    </a>
-                    </div>
-                </div>
-                <hr>
-                <div class="post-preview">
-                    <div class="invalides" >
-                    <a href="invalides.html">
-                        <h2 class="post-title">
-                            Pack Travel
-                        </h2>
-                        <h3 class="post-subtitle">
-                            Quand t'as la Flem
-                        </h3>
-                    </a>
-                    </div>
-                </div>
-                <hr>
-                <div class="post-preview">
-                    <div class="mirabeau" >
-                    <a href="mirabeau.html">
-                        <h2 class="post-title">
-                            Pack Hotel
-                        </h2>
-                        <h3 class="post-subtitle">
-                            Quand t'es pas d'ici
-                        </h3>
-                    </a>
-                    </div>
-                </div>
-                <hr>
-                <div class="post-preview">
-                    <div class="neuf" >
-                    <a href="neuf.html">
-                        <h2 class="post-title">
-                            Pack VIP
-                        </h2>
-                        <h3 class="post-subtitle">
-                            Quand t'es pété de thune
-                        </h3>
-                    </a>
+        <div class="cover">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="row">
+                    <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1">
+                        <div class="post-preview ">
+                            <div class="bouton-ticket col-md-6" >
+                                <a href="http://www.digitick.com/dream-nation-festival-after-techno-parade-soiree-les-docks-de-paris-aubervilliers-24-septembre-2016-css4-digitick-pg101-ri3965817.html?utm_source=http%3A%2F%2Fwww.digitick.com&utm_medium=patchwork&utm_campaign=23052016patchworkDreamNationAfterTechnoParade&gclid=CjwKEAjwgo6_BRC32q6_5s2R-R8SJAB7hTG-bRYHhtDSQDSsvNLfAaaaJIsacdRVzqa3Yzop8VuqUxoC_RDw_wcB">
+                                    <h2 class="post-title">
+                                        Early Tickets
+                                    </h2>
+                                    <h3 class="post-subtitle">
+                                        Jusqu'au 27/09
+                                    </h3>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="post-preview">
+                            <div class="bouton-ticket col-md-6" >
+                                <a href="http://www.digitick.com/dream-nation-festival-after-techno-parade-soiree-les-docks-de-paris-aubervilliers-24-septembre-2016-css4-digitick-pg101-ri3965817.html?utm_source=http%3A%2F%2Fwww.digitick.com&utm_medium=patchwork&utm_campaign=23052016patchworkDreamNationAfterTechnoParade&gclid=CjwKEAjwgo6_BRC32q6_5s2R-R8SJAB7hTG-bRYHhtDSQDSsvNLfAaaaJIsacdRVzqa3Yzop8VuqUxoC_RDw_wcB">
+                                    <h2 class="post-title">
+                                        Pack Travel
+                                    </h2>
+                                    <h3 class="post-subtitle">
+                                        Quand t'as la Flem
+                                    </h3>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="post-preview">
+                            <div class="bouton-ticket col-md-6" >
+                                <a href="http://www.digitick.com/dream-nation-festival-after-techno-parade-soiree-les-docks-de-paris-aubervilliers-24-septembre-2016-css4-digitick-pg101-ri3965817.html?utm_source=http%3A%2F%2Fwww.digitick.com&utm_medium=patchwork&utm_campaign=23052016patchworkDreamNationAfterTechnoParade&gclid=CjwKEAjwgo6_BRC32q6_5s2R-R8SJAB7hTG-bRYHhtDSQDSsvNLfAaaaJIsacdRVzqa3Yzop8VuqUxoC_RDw_wcB">
+                                    <h2 class="post-title">
+                                        Pack Hotel
+                                    </h2>
+                                    <h3 class="post-subtitle">
+                                        Quand t'es pas d'ici
+                                    </h3>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="post-preview">
+                            <div class="bouton-ticket col-md-6" >
+                                <a href="http://www.digitick.com/dream-nation-festival-after-techno-parade-soiree-les-docks-de-paris-aubervilliers-24-septembre-2016-css4-digitick-pg101-ri3965817.html?utm_source=http%3A%2F%2Fwww.digitick.com&utm_medium=patchwork&utm_campaign=23052016patchworkDreamNationAfterTechnoParade&gclid=CjwKEAjwgo6_BRC32q6_5s2R-R8SJAB7hTG-bRYHhtDSQDSsvNLfAaaaJIsacdRVzqa3Yzop8VuqUxoC_RDw_wcB">
+                                    <h2 class="post-title">
+                                        Pack VIP
+                                    </h2>
+                                    <h3 class="post-subtitle">
+                                        Quand t'es pété de thune
+                                    </h3>
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
     <?php
     include "footer.php";
