a54dd6e62dbf014a2c2ec9067e3351608f540a04
Hyce <jon.poullain@gmail.com>
2016-09-23 18:04:33 +0200

    links footer

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 <ul class="list-inline text-center">
                     <li>
-                        <a href="">
+                        <a href="https://twitter.com/DreamNationFest">
                             <span class="fa-stack fa-lg">
                                 <i class="fa fa-circle fa-stack-2x"></i>
                                 <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
@@ -13,7 +13,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="">
+                        <a href="https://www.facebook.com/dreamnationfestival/?fref=ts">
                             <span class="fa-stack fa-lg">
                                 <i class="fa fa-circle fa-stack-2x"></i>
                                 <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
@@ -21,7 +21,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="#">
+                        <a href="https://github.com/kimerthen/A2_PREPRO_BALLERIN_POULLAIN">
                             <span class="fa-stack fa-lg">
                                 <i class="fa fa-circle fa-stack-2x"></i>
                                 <i class="fa fa-github fa-stack-1x fa-inverse"></i>
